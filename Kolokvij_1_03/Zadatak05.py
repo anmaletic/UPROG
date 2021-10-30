0a4a750c410d340cafb1e768a6301eaad8fbb65f
AnMal <junkie_@msn.com>
2021-10-30 15:32:27 +0200

    k3_z5

---
+++ Kolokvij_1_03/Zadatak05.py
@@ -0,0 +1,16 @@
+for i in range(12, 2, -4):
+    print(i)
+
+for i in range(4,5):
+    print(i)
+
+for i in range(5):
+    print('***', end = 'X')
+
+# 12
+#  8
+#  4
+ 
+#  4
+
+# ***X***X***X***X***X
