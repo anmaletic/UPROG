8b3f1cf9e5ac0b0f01bd8086b51e22363944f9fe
AnMal <junkie_@msn.com>
2021-10-30 16:00:23 +0200

    k3_z9

---
+++ Kolokvij_1_03/Zadatak09.py
@@ -0,0 +1,13 @@
+# Što će se ispisati?
+
+for i in range(4):              # 0 1 2 3
+    for j in range(i):          # 0 1 2
+        print(i, end = '')
+        print()
+
+# 1
+# 2
+# 2
+# 3
+# 3
+# 3
