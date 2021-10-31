e0f8042c63cc6f6c56f9a50a4bce79cf5090084b
AnMal <junkie_@msn.com>
2021-10-31 15:53:33 +0100

    p_05_z02

--- Predavanje_05_z02.py
+++ Predavanje_05_z02.py
@@ -3,14 +3,14 @@
 # dijeljenja i ostatak cijelobrojnog dijelenja ta dva broja. Koristite while petlju, 
 # naredbu break i funkciju divmod()
 
-unosBroj1 = int(input("Unesite prvi cijeli pozitivni broj: "))
-unosBroj2 = int(input("Unesite drugi cijeli pozitivni broj: "))
+
 
 while True:
+    unosBroj1 = int(input("Unesite prvi cijeli pozitivni broj: "))
+    unosBroj2 = int(input("Unesite drugi cijeli pozitivni broj: "))
+    
     if(unosBroj1 < 1 or unosBroj2 < 1):
         print("Brojevi nisu prirodni, pokušajte ponovo.")
-        unosBroj1 = int(input("Unesite prvi cijeli pozitivni broj: "))
-        unosBroj2 = int(input("Unesite drugi cijeli pozitivni broj: "))
     else:
         rezultat, ostatak = divmod(unosBroj1, unosBroj2)
         print(F"Rezultat djeljenja {unosBroj1} i {unosBroj2} je {rezultat}, a ostatak {ostatak}.")
