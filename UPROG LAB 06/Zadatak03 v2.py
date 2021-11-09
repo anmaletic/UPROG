9fcdde3ca25246eaaf33138c0f112d6e5241f922
AnMal <junkie_@msn.com>
2021-11-09 17:23:00 +0100

    commit

---
+++ UPROG LAB 06/Zadatak03 v2.py
@@ -0,0 +1,23 @@
+# Napišite funkciju potencija koja vraća vrijednost izraza baza**eksponent.
+# Npr. za bazu 2 i eksponent 3 funkcija vraća 8 ( 2*2*2). 
+# Pretpostavite da su baza i eksponent pozitivne cjelobrojne vrijednosti različite od nule. 
+# Napišite program koji od korisnika traži unos vrijednosti za bazu i eksponent te poziva funkciju 
+# potencija i prosljeđuje joj te dvije vrijednosti. Unutar funkcije upotrijebite for petlju za 
+# ponavljajuće izvođenje izraza unutar petlje. Broj ponavljanja treba biti jednak vrijednosti 
+# proslijeđenog argumenta eksponent. Funkcija vraća vrijednost izračuna.
+
+def potencija(baza, eksponent):
+    for i in range(eksponent):
+        if(i == 0):
+            pot = baza
+        else:
+            pot *= baza
+
+    return pot
+
+unosBaza = int(input("Unesite bazu: "))
+unosEksp = int(input("Unesite eksponent: "))
+
+result = potencija(unosBaza, unosEksp)
+
+print(F"{unosBaza} ** {unosEksp} = {result}")
