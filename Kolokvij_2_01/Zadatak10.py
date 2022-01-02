901e0db5925f7bd878e84aee9eea5a0a0d9c2205
AnMal <junkie_@msn.com>
2022-01-02 15:00:19 +0100

    commit

---
+++ Kolokvij_2_01/Zadatak10.py
@@ -0,0 +1,4 @@
+# Napišite program koji će unositi jednu rečenicu i promijeniti je na način da se svako 
+# pojavljivanje znakovnog niza 'ije' ili 'je' zamijeni sa znakom '?' . Potom se ta nova rečenica 
+# ispisuje.
+
