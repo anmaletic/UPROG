9e15948158db7aa913e96c33de7bcef28d634471
AnMal <junkie_@msn.com>
2022-01-02 15:07:19 +0100

    commit

--- Kolokvij_2_01/Zadatak06.py
+++ Kolokvij_2_01/Zadatak06.py
@@ -1,6 +1,6 @@
 # Zaokružite broj ispred naredbi koje nisu napisane prema pravilima dobrog stila pisanja u 
 # Pythonu i desno napišite kako bi naredba trebala izgledati:
-# 1. x =y//2                                    x = x // 2
+# 1. x =y//2                                    x = y // 2
 # 2. y+=abs (a)                                 y += abs(a)
 # 3. print('{} + {} = {}'.format(a,b,c))        print('{} + {} = {}'.format(a, b, c)) 
 # 4. n<=m                                       n <= m
